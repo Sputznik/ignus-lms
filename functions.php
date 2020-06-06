de79e1209f212dc5752c477d95705b4d9f652e62
guneetnarula <guneetnarula@sputznik.com>
2020-06-05 22:58:28 -0700

    Force from-email and name change

--- functions.php
+++ functions.php
@@ -1,3 +1,18 @@
 <?php
-  define ( 'ignus-lms-version', '0.1' );
+  define ( 'ignus-lms-version', '0.1.1' );
+
+  // Function to change email address
+  function wpb_sender_email( $original_email_address ) {
+    return 'mail@ignus.academy';
+  }
+
+  // Function to change sender name
+  function wpb_sender_name( $original_email_from ) {
+      return 'Ignus Academy';
+  }
+
+  // Hooking up our functions to WordPress filters
+  add_filter( 'wp_mail_from', 'wpb_sender_email' );
+  add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
+
 ?>
