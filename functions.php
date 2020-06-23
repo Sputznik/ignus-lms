66d9947804b4e9e9d03144aa9cf1e03fb87c8a61
guneetnarula <guneetnarula@sputznik.com>
2020-06-23 11:03:45 -0700

    Added Google Analytics script

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-  define ( 'ignus-lms-version', '0.1.1' );
+  define ( 'ignus-lms-version', '0.1.2' );
 
   // Function to change email address
   function wpb_sender_email( $original_email_address ) {
@@ -14,5 +14,19 @@
   // Hooking up our functions to WordPress filters
   add_filter( 'wp_mail_from', 'wpb_sender_email' );
   add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
+
+  function ign_google_analytics() { ?>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170499175-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-170499175-1');
+      </script>
+      <?php
+  }
+
+  add_action( 'wp_head', 'ign_google_analytics', 10 );
 
 ?>
